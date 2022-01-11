31750ee20c8e0dc6b3d466986f6b31d98873bc0b
henrythier <henry@thierhoff.rocks>
2022-01-11 19:24:07 +0100

    added tesla ids

--- code/config.py
+++ code/config.py
@@ -3,5 +3,9 @@ config = {
     "individual_station_url":"https://ladesaeulenregister.de/ajax?ids={}&l=0&r=100&t=60&b=40",
     "station_ids_fp":"../data/stations_id.csv",
     "points_data_fp":"../data/points_data.json",
-    "stations_data_fp":"../data/stations_data.json"
+    "stations_data_fp":"../data/stations_data.json",
+    "tesla_dc_id_url":"https://www.tesla.com/findus/list/chargers/Germany",
+    "tesla_dc_id_fp":"../data/tesla_dc_id.csv",
+    "tesla_sc_id_url":"https://www.tesla.com/findus/list/superchargers/Germany",
+    "tesla_sc_id_fp":"../data/tesla_sc_id.csv"
     }
---
+++ code/get_tesla_sc_ids.py
@@ -0,0 +1,22 @@
+import requests
+from config import config
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = config["tesla_sc_id_url"]
+fp = config["tesla_sc_id_fp"]
+
+r = requests.get(url)
+
+if r.status_code != 200:
+    raise Exception(f'Request returned an error: {r.status_code} {r.text}')
+
+html_doc = r.text
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+links = [a['href'] for a in soup.find_all('a', href=True)]
+
+ids = [link.split('/')[-1] for link in links if 'charger' in link]
+
+df = pd.DataFrame(columns=['id'], data=ids)
+df.to_csv(fp, index=None)
